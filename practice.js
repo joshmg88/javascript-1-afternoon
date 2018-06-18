586d9e1de3a624358a9d9af3d77bf1a6b1d63d6b
joshmg88 <joshmg88@gmail.com>
2018-06-18 15:11:29 -0500

    initial commit

--- practice.js
+++ practice.js
@@ -5,28 +5,32 @@
 //greeting should return the string "Hello, "
 //plus the value of the name parameter.
 
-//Code here
+function greeting(name) {
+  return "Hello, " + name;
+}
 
 //////////////////PROBLEM 2////////////////////
 
 //Rewrite the function greeting as a function expression.
 //Name it newGreeting.
 
-//Code Here
+var newGreeting = function (name) {
+  return "Hello, " + name;
+}
 
 //////////////////PROBLEM 3////////////////////
 
 //Rewrite the function greeting as an arrow function.
 //Name it finalGreeting.
 
-//Code Here
+var finalGreeting = name => "Hello, " + name;
 
 //////////////////PROBLEM 4////////////////////
 
 //Create an array called groceries with the values
 //"apples", "milk", "eggs", "bread"
 
-//Code Here
+var groceries = ["apples", "milk", "eggs", "bread"];
 
 //Write a function called doubleCheck that takes in an array
 //as a parameter.
@@ -34,7 +38,27 @@
 //If the array does not contain "chocolate", add "chocolate".
 //doubleCheck should return the array.
 
-//Code Here
+//  function doubleCheck(array){
+//   for(let i=0; i<=array.length; i++){
+//     if(array[i] !== "chocolate"){
+//       array.push("chocolate")
+//     }
+//     return array;
+//   }
+// }
+
+function doubleCheck(array) {
+  var hasCoco = false;
+  for (let i = 0; i <= array.length; i++) {
+    if (array[i] === "chocolate") {
+      hasCoco = true
+    }
+    if(hasCoco === false){
+      array.push("chocolate")
+    }
+  }
+  return array;
+}
 
 //////////////////PROBLEM 5////////////////////
 
@@ -43,21 +67,20 @@
 //name (a string), color: (a string), age (a number),
 //and goodBoy (a boolean).
 
-//Code Here
+var dog = {name: "Sadie", color: "black", age: 5, goodBoy: true};
 
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
 
-//Code Here
+var devMountainClassPet = dog.name;
 
 //Add a method to dog called bark.
 //The value of bark should be a function that returns the string "Woof woof".
-
-//Code Here
+dog.bark = function(){return "Woof woof"};
 
 //Store the result of invoking the bark method in a variable called ruff.
 
-//Code Here
+var ruff = dog.bark();
 
 //////////////////PROBLEM 6////////////////////
 
@@ -70,8 +93,16 @@
 
 //Return mySum.
 
-//Code Here
-
+function looper(array){
+  var mySum = 0;
+  for(let i=0; i<=array.length; i++){
+    if(array[i]%2 !== 0 || array[i] >= 100){
+       mySum + array[i];
+    }
+  }
+  return mySum;
+}
+console.log(looper([1,2,3,5,100,1000,6]))
 //////////////////PROBLEM 7////////////////////
 
 //Given the following function called math
